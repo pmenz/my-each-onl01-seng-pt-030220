5f8cd813a2f7a96e9714fbf276cfd3878e33e4fd
Pablo Menz <p.menz.r@gmail.com>
2020-03-12 17:22:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,12 @@
 def my_each(a)
   i=0
   
-  if a.lenght == 0 
+  if a.length == 0 
     puts a
     return
   
   else
-    while i < a.lenght
+    while i < a.length
     puts a[i]
     i += 1
     
