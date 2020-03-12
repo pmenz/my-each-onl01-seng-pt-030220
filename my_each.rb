12f19820a4c90c6cb9da9708746e174bfe316a52
Pablo Menz <p.menz.r@gmail.com>
2020-03-12 18:32:05 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,10 +2,10 @@ def my_each(a)
 i=0 
 if block_given? then  
   puts "This block should not run!"
-return 
+
  elsif
      while i < a.lenght
      yield (puts a[i] )
      i += 1  
-     end
+     
    end
